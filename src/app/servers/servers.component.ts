69f799c33120a290f7f3dc2fdbeb258734f4e3ed
Maria A Perez <mperez@wbinnova.com>
2019-08-07 16:13:39 -0400

    Assignment 1

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  //Option 1
+  selector: 'app-servers', //should be in this way
+
+  //Option 2
+  //selector: '[app-servers]',
+
+  //Option 3
+  //selector: '.app-servers',
+
   //templateUrl: './servers.component.html',
   template: 
     '<app-server></app-server><app-server></app-server>',
